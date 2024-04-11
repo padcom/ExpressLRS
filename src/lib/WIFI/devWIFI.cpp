2ea40abb6b80bb580643e79c71d0fad5674b72ad
Matthias Hryniszak <padcom@gmail.com>
2024-04-11 12:49:41 +0200

    Fixed build

--- src/lib/WIFI/devWIFI.cpp
+++ src/lib/WIFI/devWIFI.cpp
@@ -157,6 +157,14 @@ static struct {
   {"/index.css", (uint8_t *)INDEX_CSS, sizeof(INDEX_CSS)},
 };
 
+String getContentType(const String& path){
+  if (path.endsWith(".html")) return "text/html";
+  else if (path.endsWith(".css")) return "text/css";
+  else if (path.endsWith(".js")) return "application/javascript";
+  else if (path.endsWith(".json")) return "application/json";
+  else return "text/plain";
+}
+
 static void WebUpdateSendContent(AsyncWebServerRequest *request)
 {
   for (size_t i=0 ; i<ARRAY_SIZE(files) ; i++) {
